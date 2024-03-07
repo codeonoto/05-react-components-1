c4cb8ee84eb8ff342ef2e3e52b13d90d7b841d9d
codeonoto <onoto0409@gmail.com>
2024-03-07 20:24:53 +0530

    Refactor MovieList and MovieCard components

--- src/MovieList.jsx
+++ src/MovieList.jsx
@@ -5,19 +5,82 @@ class MovieList extends React.Component {
   constructor() {
     super();
     this.state = {
-      title: 'Avengers Endgame',
-      plot: 'The Avengers Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aut a laudantium.',
-      price: 199,
-      rating: 8.9,
-      stars: 0,
-      fav: false,
-      cart: false,
+      movies: [
+        {
+          title: 'The Avengers',
+          plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+          poster:
+            'https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg',
+          rating: '8.0',
+          price: 99,
+          stars: 0,
+          fav: false,
+          cart: false,
+        },
+        {
+          title: 'The Dark Knight',
+          plot: 'When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+          poster:
+            'https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg',
+          rating: '9.0',
+          price: 199,
+          stars: 0,
+          fav: false,
+          cart: false,
+        },
+        {
+          title: 'Iron Man',
+          plot: 'After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.',
+          poster:
+            'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg',
+          rating: '7.9',
+          price: 139,
+          stars: 0,
+          fav: false,
+          cart: false,
+        },
+      ],
     };
   }
+
+  handleIncStar = (movie) => {
+    const { movies } = this.state;
+    const mId = movies.indexOf(movie);
+    if (movies[mId].stars >= 5) {
+      return;
+    }
+    movies[mId].stars += 0.5;
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleDecStar = (movie) => {
+    const { movies } = this.state;
+    const mId = movies.indexOf(movie);
+    if (movies[mId].stars <= 0) {
+      return;
+    }
+    movies[mId].stars -= 0.5;
+
+    this.setState({
+      movies,
+    });
+  };
+
   render() {
+    const { movies } = this.state;
+
     return (
       <>
-        <MovieCard movie={this.state} />
+        {movies.map((movie) => (
+          <MovieCard
+            movies={movie}
+            addStars={this.handleIncStar}
+            minusStars={this.handleDecStar}
+          />
+        ))}
       </>
     );
   }
--- src/Moviecard.jsx
+++ src/Moviecard.jsx
@@ -2,52 +2,19 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   // Add the addStars method here
-  addStars() {
-    if (this.state.stars >= 5) {
-      return;
-    }
-    this.setState((prevState) => {
-      return {
-        stars: prevState.stars + 0.5,
-      };
-    });
-  }
-
-  //
-  minusStars() {
-    if (this.state.stars > 0) {
-      this.setState((prevState) => {
-        return {
-          stars: prevState.stars - 0.5,
-        };
-      });
-    }
-  }
-
-  handleFav = () => {
-    this.setState({
-      fav: !this.state.fav,
-    });
-  };
-
-  handleCart = () => {
-    this.setState({
-      cart: !this.state.cart,
-    });
-  };
 
   render() {
     console.log(this.props);
-    const { movie: data } = this.props;
+    const { addStars, minusStars, movies, movies: data } = this.props;
     console.log(data);
-    const { title, plot, price, rating, stars, fav, cart } = data;
+    const { title, poster, plot, price, rating, stars, fav, cart } = data;
 
     return (
       <div className='main'>
         <div className='movie-card'>
           <div className='left'>
             <img
-              src='https://rukminim2.flixcart.com/image/850/1000/jxhv1jk0/poster/c/x/v/medium-thor-avengers-endgame-new-poster-for-room-office-endgame-original-imafgyfefdnvuvjh.jpeg?q=90&crop=false'
+              src={poster}
               alt='Poster'
             />
           </div>
@@ -64,7 +31,7 @@ class MovieCard extends React.Component {
                   src='https://cdn-icons-png.flaticon.com/128/1828/1828779.png'
                   alt='decrease'
                   className='str-btn'
-                  onClick={this.minusStars.bind(this)}
+                  onClick={() => {minusStars(movies)}}
                 />
                 <img
                   src='https://cdn-icons-png.flaticon.com/128/1828/1828884.png'
@@ -75,23 +42,13 @@ class MovieCard extends React.Component {
                   src='https://cdn-icons-png.flaticon.com/128/10948/10948015.png'
                   alt='increase'
                   className='str-btn'
-                  onClick={this.addStars.bind(this)}
+                  onClick={() => {
+                    addStars(movies);
+                  }}
                 />
                 <span>{stars}</span>
               </div>
-              {/* {fav ? (
-                <button
-                  className='unfavourite-btn'
-                  onClick={this.handleFav}>
-                  UnFavourite
-                </button>
-              ) : (
-                <button
-                  className='favourite-btn'
-                  onClick={this.handleFav}>
-                  Favourite
-                </button>
-              )} */}
+
               <button
                 className={fav ? 'unfavourite-btn' : 'favourite-btn'}
                 onClick={this.handleFav}>
